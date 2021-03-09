06dab7e89d52a4a0d208117ef1d0dd2b663817c5
Manuel Quiñones <manuel.por.aca@gmail.com>
2021-03-09 09:56:29 -0300

    Setup: Ensure yarn version in bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,6 +4,6 @@
 
 pipenv install
 pipenv run nodeenv -p --node=10.17.0
-pipenv run npm install -g yarn
+pipenv run npm install -g yarn@1.22.10
 pipenv run yarn global add @vue/cli
 pipenv run yarn install
