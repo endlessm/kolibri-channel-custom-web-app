864ccb076a0c4c86c7a1e46d0b08726bbd6cc5f2
Manuel Quiñones <manuel.por.aca@gmail.com>
2021-03-08 21:29:14 -0300

    scripts: Copy all bundles from the template
    The template now builds and bundles multiple zip files, so the
    copy_bundles.py script is now adapted to copy all those zip files to
    the corresponding destination. For example, an override named `foo`
    will be bundled as `template-ui/foo.zip` and will be copied to:
    
    ```
    DESTINATION/foo/custom-channel-ui.zip
    ```
    
    Which is how the Explore plugin expects it.
    
    Also: add description to the script to be displayed by calling --help.
    
    https://phabricator.endlessm.com/T31531

--- scripts/copy_bundles.py
+++ scripts/copy_bundles.py
@@ -5,9 +5,9 @@ import shutil
 import subprocess
 import json
 
-from _common import TEMPLATE_WORKSPACE
+from _common import TEMPLATE_WORKSPACE, get_available_overrides
 
-ZIP_FILENAME = 'custom-channel-ui.zip'
+DEFAULT_ZIP_FILENAME = 'custom-channel-ui.zip'
 SKIP_WORKSPACES = ['kolibri-api', TEMPLATE_WORKSPACE]
 
 
@@ -19,7 +19,7 @@ def validate_dir_path(path):
 
 
 def get_workspaces_info():
-    result = subprocess.run(['yarn', 'workspaces', 'info'],
+    result = subprocess.run(['yarn', 'workspaces', '--json', 'info'],
                             stdout=subprocess.PIPE)
     output = result.stdout.decode('utf-8')
     return json.loads(output)
@@ -35,25 +35,31 @@ def workspace_to_appname(workspace):
     return '-'.join(workspace.split('-')[:-1])
 
 
-def copy_bundle_zip(workspace, dest_path, app_name=None):
-    bundle_zip_path = os.path.join(workspace, ZIP_FILENAME)
+def copy_bundle_zip(workspace, dest_path, app_name=None,
+                    zip_name=DEFAULT_ZIP_FILENAME):
+    bundle_zip_path = os.path.join(workspace, zip_name)
     if not os.path.exists(bundle_zip_path):
         print(f'Skipping {workspace}, zip not found.')
         return
-    print(f'Copying {workspace}...')
     if app_name is None:
         app_name = workspace_to_appname(workspace)
+    print(f'Copying presentation for {app_name}...')
     dest_zip_path = os.path.join(dest_path, app_name)
     if not os.path.exists(dest_zip_path):
         os.mkdir(dest_zip_path)
-    shutil.copy(bundle_zip_path, dest_zip_path)
+    shutil.copy(bundle_zip_path,
+                os.path.join(dest_zip_path, DEFAULT_ZIP_FILENAME))
 
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+    description='Copy all ZIP bundles to the desired destination path.',
+)
 parser.add_argument('dest_path', type=validate_dir_path)
 args = parser.parse_args()
 
 for workspace in get_workspaces():
     copy_bundle_zip(workspace, args.dest_path)
 
-copy_bundle_zip(TEMPLATE_WORKSPACE, args.dest_path, 'default')
+for override in get_available_overrides():
+    zip_name = f'{override}.zip'
+    copy_bundle_zip(TEMPLATE_WORKSPACE, args.dest_path, override, zip_name)
