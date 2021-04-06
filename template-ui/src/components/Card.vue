db852976c76df0ac91ca193e01ab9754993cc3dd
Manuel Quiñones <manuel.por.aca@gmail.com>
2021-04-06 12:58:52 -0300

    template: Split card in high-res and regular variants
    The Card component is now a proxy. This is to make room to more
    variants.
    
    https://phabricator.endlessm.com/T31724

--- template-ui/src/components/Card.vue
+++ template-ui/src/components/Card.vue
@@ -1,65 +1,25 @@
 <template>
-  <b-link
-    v-on:click="isLeaf? goToContent(node) : false"
-    :to="isLeaf? '' : getTopicUrl(node)"
-    class="text-reset text-decoration-none"
-    v-b-hover="handleHover"
-  >
-    <b-card
-      :img-src="thumbnail"
-      :img-alt="node.title"
-      :class="{
-        shadow: isHovered,
-        'my-2': !isHighQualityMedia,
-        'is-high-quality': isHighQualityMedia,
-      }"
-      :overlay="isHighQualityMedia"
-    >
-      <span class="font-weight-bold">{{ title }}</span>
-    </b-card>
-  </b-link>
+  <component
+    :is="cardVariant"
+    :node="node"
+  />
 </template>
 
 <script>
 import { mapState } from 'vuex';
-import cardMixin from '@/components/mixins/cardMixin';
 
 export default {
   props: ['node'],
-  mixins: [cardMixin],
   computed: {
     ...mapState(['isHighQualityMedia']),
+    cardVariant() {
+      switch (this.isHighQualityMedia) {
+        case true:
+          return 'HighResCard';
+        default:
+          return 'RegularCard';
+      }
+    },
   },
 };
 </script>
-
-<style lang="scss" scoped>
-@import '@/styles.scss';
-
-.card {
-  transition: all ease .4s;
-  &:hover {
-    color: $primary;
-  }
-  &.is-high-quality {
-    border: 0;
-  }
-}
-
-.card-img-overlay {
-  transition: all ease .4s;
-  color: $white;
-  display: flex;
-  align-items: flex-end;
-  padding: 0;
-  &:hover {
-    color: $primary;
-  }
-  span {
-    background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.7) 15%);
-    width: 100%;
-    padding: $card-img-overlay-padding;
-  }
-}
-
-</style>
---
+++ template-ui/src/components/RegularCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-link
+    v-on:click="isLeaf? goToContent(node) : false"
+    :to="isLeaf? '' : getTopicUrl(node)"
+    class="text-reset text-decoration-none"
+    v-b-hover="handleHover"
+  >
+    <b-card
+      :img-src="thumbnail"
+      :img-alt="node.title"
+      class="my-2"
+      :class="{
+        shadow: isHovered,
+      }"
+    >
+      <span class="font-weight-bold">{{ title }}</span>
+    </b-card>
+  </b-link>
+</template>
+
+<script>
+import cardMixin from '@/components/mixins/cardMixin';
+
+export default {
+  props: ['node'],
+  mixins: [cardMixin],
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles.scss';
+
+.card {
+  transition: all ease .4s;
+  &:hover {
+    color: $primary;
+  }
+}
+
+</style>
