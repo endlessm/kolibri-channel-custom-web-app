cc9872e420273826ff51aab65cea186b12e55420
Manuel Quiñones <manuel.por.aca@gmail.com>
2021-04-21 17:27:05 -0300

    template: Add initial empty results for filters
    Simple plain text for now.
    
    https://phabricator.endlessm.com/T31817

--- template-ui/src/views/Home.vue
+++ template-ui/src/views/Home.vue
@@ -6,21 +6,30 @@
     <Carousel />
     <FilterContent />
 
-    <CardGrid
-      :nodes="contentNodes"
-      v-if="contentNodes"
-    />
+    <div v-if="isFilterResultEmpty">
+      <EmptyResultsMessage />
+    </div>
 
-    <CardGrid
-      v-for="section in filteredSections"
-      :key="section.id"
-      :nodes="filterNodes(section.children)"
-      :id="section.id"
-    >
-      <b-row>
-        <SectionTitle :section="section" />
-      </b-row>
-    </CardGrid>
+    <template v-else>
+      <CardGrid
+        :nodes="contentNodes"
+        v-if="contentNodes"
+      />
+      <div
+        v-for="section in filteredSections"
+        :key="section.id"
+      >
+        <CardGrid
+          v-if="filterNodes(section.children).length"
+          :nodes="filterNodes(section.children)"
+          :id="section.id"
+        >
+          <b-row>
+            <SectionTitle :section="section" />
+          </b-row>
+        </CardGrid>
+      </div>
+    </template>
 
   </div>
 </template>
@@ -49,6 +58,13 @@ export default {
     },
     filteredSections() {
       return this.filterNodes(this.mainSections);
+    },
+    isFilterResultEmpty() {
+      const isContentNodeEmpty = this.contentNodes === null || this.contentNodes.length === 0;
+      const anySectionNode = this.filteredSections.some(
+        (s) => (this.filterNodes(s.children).length),
+      );
+      return isContentNodeEmpty && !anySectionNode;
     },
   },
 };
