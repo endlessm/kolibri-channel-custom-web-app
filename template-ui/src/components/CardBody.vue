0c44af91585133dab5806842acdb32db71b2dcf4
Manuel Quiñones <manuel.por.aca@gmail.com>
2021-04-27 19:42:43 -0300

    template: Add info line to cards
    With the type + grade or level, as specified.
    
    https://phabricator.endlessm.com/T31813

--- template-ui/src/components/CardBody.vue
+++ template-ui/src/components/CardBody.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <p class="text-uppercase text-info mb-1">
+      <span v-if="typeTag">{{ typeTag }}</span>
+      <span v-if="typeTag && gradeOrLevelTag"> • </span>
+      <span v-if="gradeOrLevelTag">{{ gradeOrLevelTag }}</span>
+    </p>
     <p class="mb-0 font-weight-bold">{{ node.title }}</p>
     <p class="mb-0">{{ getCardSubtitle(node) }}</p>
     <b-badge
@@ -21,9 +26,21 @@ export default {
   props: ['node'],
   computed: {
     ...mapGetters(['getCardSubtitle']),
-    ...mapGetters({ getStructuredTags: 'filters/getStructuredTags' }),
+    ...mapGetters({
+      getStructuredTags: 'filters/getStructuredTags',
+      getFirstStructuredTag: 'filters/getFirstStructuredTag',
+    }),
     subjectTags() {
       return this.getStructuredTags(this.node, StructuredTags.SUBJECT);
+    },
+    typeTag() {
+      return this.getFirstStructuredTag(this.node, StructuredTags.TYPE);
+    },
+    gradeOrLevelTag() {
+      return (
+        this.getFirstStructuredTag(this.node, StructuredTags.GRADE)
+        || this.getFirstStructuredTag(this.node, StructuredTags.LEVEL)
+      );
     },
   },
 };
