12a7679d3f2734a4bbbd03b672f325479773573c
danigm <daniel@endlessm.com>
2021-05-14 04:58:43 -0700

    Update pbskids-ui/src/utils.js
    Co-authored-by: manuq <manuel.por.aca@gmail.com>

--- pbskids-ui/src/utils.js
+++ pbskids-ui/src/utils.js
@@ -2,7 +2,7 @@ export default function getSlug(title) {
   return title
     .toLowerCase()
     .replace(/&/g, 'and')
-    // Remove invalid chars
+    // Remove invalid characters
     .replace(/[^a-z0-9 -]/g, '')
     // Replace whitespace by -
     .replace(/\s+/g, '-')
